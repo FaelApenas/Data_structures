b80a70ae2793e99a5680a19f2c2a04196d07aff3
Rafael Nogueira Serpa <rafaeln.serpa@gmail.com>
2024-01-13 00:07:29 -0300

    Setup for code

---
+++ Hash_table/hash.c
@@ -0,0 +1,12 @@
+#include "hash.h"
+
+      
+
+
+int main(int argc, char**argv)
+{
+	printf("Hello World" );
+
+
+	return 0; 
+}
---
+++ Hash_table/hash.h
@@ -0,0 +1,11 @@
+#ifdef HASH_H 
+#define HASH_H
+
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h> 
+
+
+
+#endif
